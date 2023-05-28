5ecea9ba2021e5257b8e483e2f176504f30c3011
Defochka <94522772+Defochka@users.noreply.github.com>
2023-05-28 12:30:19 +0300

    Note.kt

--- app/src/main/java/com/topic2/android/notes/util/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/util/components/Note.kt
@@ -1,80 +1,35 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.topic2.android.notes.R
 import com.topic2.android.notes.theme.rwGreen
-import com.topic2.android.notes.util.components.NoteColor
 
 @Composable
-fun Note(){
-    val backgroundShape: Shape = RoundedCornerShape(4.dp)
-    Row(
-        modifier = Modifier
-            .padding(8.dp)
-            .shadow(1.dp, backgroundShape)
-            .fillMaxWidth()
-            .heightIn(min = 64.dp)
-            .background(Color.White, backgroundShape)
-    ) {
-        NoteColor(
-            modifier = Modifier.align(Alignment.CenterVertically)
-                .padding(start = 16.dp, end = 16.dp),
-            color = rwGreen ,
-            size = 40.dp,
-            padding = 4.dp,
-            border = 1.dp
-        )
-        Column(
+fun Note() {
+    Row(modifier = Modifier.fillMaxSize()) {
+
+        Box(
             modifier = Modifier
-                .weight(1f)
-                .align(Alignment.CenterVertically)
-        ) {
-            Text(
-                text = stringResource(id = R.string.header),
-                color = Color.Black,
-                maxLines = 1,
-                style = TextStyle(
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 16.sp,
-                    letterSpacing = 0.15.sp
-                )
-            )
-            Text(
-                text = stringResource(id = R.string.content),
-                color = Color.Black.copy(alpha = 0.75f),
-                maxLines = 1,
-                style = TextStyle(
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 14.sp,
-                    letterSpacing = 0.25.sp
-                )
-            )
+                .size(40.dp)
+                .background(rwGreen)
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Загловок", maxLines = 1)
+            Text(text = "Содержание", maxLines = 1)
         }
         Checkbox(
             checked = false,
-            onCheckedChange = { },
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
+            onCheckedChange = {},
+            modifier = Modifier.padding(start = 8.dp)
         )
     }
 }
+
 @Preview
 @Composable
-fun NotePreview(){
-    Note()
+private fun NotePreview(){ Note()
 }
